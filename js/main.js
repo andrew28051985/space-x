6d8810da504b113008003b6f9cbec2ace47f0726
andrew28051985 <korotkov915@gmail.com>
2020-08-05 23:11:10 +0300

    feat: block video ready

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var instance = new vidbg('.world', {
+  mp4: '../video/world.mp4', // URL or relative path to MP4 video
+  webm: '../video/world.webm', // URL or relative path to webm video
+  poster: '../img/world.jpg', // URL or relative path to fallback image
+  overlay: false, // Boolean to display the overlay or not
+}, {
+  // Attributes
+})
